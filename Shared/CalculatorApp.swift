0d242dba2a9db2408bc2cad5fc4afdaedb2a9dcc
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2021-05-31 10:08:48 +1000

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Szymon Lorenz on 31/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
